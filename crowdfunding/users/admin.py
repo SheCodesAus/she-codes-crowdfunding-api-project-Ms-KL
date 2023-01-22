89ab81ad470047b4a45b55dad015006393035bcb
Ms-KL <thekristyleigh@gmail.com>
2023-01-22 18:22:42 +0800

    Configured Admin Portal

--- crowdfunding/users/admin.py
+++ crowdfunding/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
