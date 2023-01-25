acdcd9f955ca55c044b7dab4928afe502d90b4b9
Ms-KL <thekristyleigh@gmail.com>
2023-01-25 22:57:06 +0800

    added projects + pledges to admin portal

--- crowdfunding/projects/admin.py
+++ crowdfunding/projects/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Project, Pledge
 
-# Register your models here.
+# # Register your models here.
+
+# -----------------------
+# ADMIN BLOCK
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title','owner', 'goal', 'is_open', 'date_created')
+
+admin.site.register(Project, ProjectAdmin)
+
+
+class PledgeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project','supporter', 'amount', 'anonymous')
+
+admin.site.register(Pledge, PledgeAdmin)
